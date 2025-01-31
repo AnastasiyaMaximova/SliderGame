13e1a34ac86385f558353bf0263a9646c36dbb03
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-01-31 20:38:40 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Anastasya Maximova on 31.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
